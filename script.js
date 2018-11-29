eae9ef73a4a833e564aa1cfa1def99e0deec802e
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2018-11-28 19:10:08 -0500

    Going home

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
+var wordList = [
+    ['pencil', 'writing'],
+    ['crayon', 'writing'],
+    ['pen', 'writing'],
+    ['paper', 'base'],
+    ['grid paper', 'base'],
+    ['glossy', 'base'],
+    ['stickman', 'subject'],
+    ['model', 'subject'],
+    ['diagram', 'subject'],
+];
+
+console.log(wordList);
+
 $(document).ready(function(){
+
     $("#q").click(function(){
         $("#hangman").attr("src", "assets/Hangman-1.png");
     });
+
+    function random(){
+        return Math.floor((Math.random() * 9))    
+    }
+
+    function wordSelect(){
+        return wordList[random()];
+    }
+
+    function start(){
+        $("#underline").text("_ ");
+        var arr = wordSelect();
+        for(var i=0; i<arr[0].length, i++;){
+            $("#underline").append("<p>_ </p>");
+        }
+        $("#clue").text("Clue: " + arr[1]);
+    }
+
+    $("#start").click(function(){
+        $(this).hide();
+        start();
+    })
 });
